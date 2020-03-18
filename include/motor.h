8bd49ffa46e507c7d8d8195604ff4181a815fab5
AbdelHady-Anka <abdelhady.anka@gmail.com>
2020-03-18 03:01:16 -0700

    first commit

--- include/motor.h
+++ include/motor.h
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
+#define motorInterfaceType 1
 
 class Motor
 {
 public:
-    Motor(uint8_t step_pin, uint8_t dir_pin, long high_band, long low_band, long init_location);
+    Motor(uint8_t step_pin, uint8_t dir_pin, long high_band, long low_band, long init_location, float speed);
     void move_to(int location);
     void init();
 
 private:
     AccelStepper stepper;
+    float speed;
     uint8_t step_pin;
     uint8_t dir_pin;
     int high_band;
@@ -18,13 +20,14 @@ private:
     String axsis_name;
 };
 
-Motor::Motor(uint8_t step_pin, uint8_t dir_pin, long high_band, long low_band, long init_location)
+Motor::Motor(uint8_t step_pin, uint8_t dir_pin, long high_band, long low_band, long init_location, float speed)
 {
     this->step_pin = step_pin;
     this->high_band = high_band;
     this->low_band = low_band;
     this->axsis_name = axsis_name;
-    this->stepper = AccelStepper(1, step_pin, dir_pin);
+    this->speed = speed;
+    this->stepper = AccelStepper(motorInterfaceType, step_pin, dir_pin);
     this->stepper.setCurrentPosition(init_location);
     // Set the maximum speed in steps per second:
     this->stepper.setMaxSpeed(1000);
@@ -32,15 +35,26 @@ Motor::Motor(uint8_t step_pin, uint8_t dir_pin, long high_band, long low_band, l
 
 void Motor::move_to(int location)
 {
-    if (location < high_band && location > low_band)
+    // Serial.println("in move to");
+    if (location <= high_band && location >= low_band)
     {
+        // Serial.println("in the if");
+        // Serial.println("current location: ");
+        // Serial.println(stepper.currentPosition());
         float shift = location - stepper.currentPosition();
         int direction = shift >= 0 ? +1 : -1;
-        while (stepper.currentPosition() != abs(shift))
+
+        // Serial.println("direction: ");
+        // Serial.println(direction);
+        while (stepper.currentPosition() != location)
         {
-            stepper.setSpeed(direction * 600);
+            stepper.setSpeed(direction * speed);
             stepper.runSpeed();
         }
+        // Serial.println("current location: ");
+        // Serial.println(stepper.currentPosition());
+        // stepper.setCurrentPosition(location);
+
         delay(1000);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,45 @@
 
 #include <motor.h>
-// Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver:
-#define dirPin 7
-#define stepPin 6
-#define motorInterfaceType 1
+// Define stepper motors connections and motor interface type. Motor interface type must be set to 1 when using a driver:
+#define stepPinY 4
+#define dirPinY 5
+
+#define stepPinX 8
+#define dirPinX 9
+
+#define stepPinZ 6
+#define dirPinZ 7
+
 #define start 10
 // Create a new instance of the Motor class:
-Motor motor = Motor(stepPin, dirPin, 5500, 0, 0);
+Motor motorY = Motor(stepPinY, dirPinY, 3625, 0, 0, 600);
+Motor motorX = Motor(stepPinX, dirPinX, 5475, 0, 0, 600);
+Motor motorZ = Motor(stepPinZ, dirPinZ, 975, 0, 0, -400);
 void setup()
 {
     pinMode(start, INPUT_PULLUP);
+    Serial.begin(9600);
 }
 void loop()
 {
     boolean val = digitalRead(start);
     if (!val)
     {
-        motor.move_to(1000);
+        motorX.move_to(5475);
+        delay(1000);
+        motorX.move_to(0);
         delay(3000);
-        motor.move_to(500);
+
+        // motorY.move_to(3625);
+        // delay(1000);
+        // motorY.move_to(0);
+
+        // delay(3000);
+
+        // motorZ.move_to(975);
+        // delay(1000);
+        // motorZ.move_to(0);
+
         val = true;
     }
 }
